59cc59bbc74da08607e4d6472de9d5f6efb9270a
wuyu201321060203 <wuyuangelysys@gmail.com>
2014-07-24 21:54:04 +0800

    (OJ)restore_binarytree

--- build_post.cc
+++ build_post.cc
@@ -4,7 +4,28 @@
 #include <stddef.h>
 #include <cassert>
 
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
 #define MAX 64
+
+typedef int TreeNodeElement;
+
+struct BinaryTreeNode
+{
+    BinaryTreeNode(BinaryTreeNode* left , BinaryTreeNode* right,
+                   TreeNodeElement value):left_(left),
+                                          right_(right),
+                                          value_(value)
+    {
+    }
+
+    BinaryTreeNode* left_;
+    BinaryTreeNode* right_;
+    TreeNodeElement value_;
+};
+
+typedef boost::function<void (BinaryTreeNode*)> VisitFunctor;
 
 void build_post(char const* pre , char const* in , int len , char* post)
 {
@@ -16,6 +37,26 @@ void build_post(char const* pre , char const* in , int len , char* post)
     build_post(pre + leftLen + 1 , in + leftLen + 1 , len - leftLen - 1,
                post + leftLen);
     post[len - 1] = pre[0];
+}
+
+void rebuild(char const* pre , char const* in , int len , BinaryTreeNode** root)
+{
+    if(pre == NULL || in == NULL || len == 0)//len is used for detecting end condition
+        return;
+    int leftLen = strchr(in , pre[0]) - in;
+    *root = new BinaryTreeNode(NULL , NULL , *pre);
+    rebuild(pre + 1 , in , leftLen , &( (*root)->left_) );
+    rebuild(pre + leftLen + 1 , in + leftLen + 1 , len - leftLen - 1,
+            &( (*root)->right_ ) );
+}
+
+void print_post_order(BinaryTreeNode const* root)
+{
+    if(root == NULL)
+        return;
+    print_post_order(root->left_);
+    print_post_order(root->right_);
+    printf("%c" , root->value_);
 }
 
 void build_post_test()
@@ -30,8 +71,21 @@ void build_post_test()
     printf("%s\n", post);
 }
 
+void rebuild_test()
+{
+    char pre[MAX] = { 0 };
+    char in[MAX] = { 0 };
+    int n;
+    BinaryTreeNode *root;
+    scanf("%s%s", pre, in);
+    n = strlen(pre);
+    rebuild(pre , in , n , &root);
+    print_post_order(root);
+}
+
 int main(void)
 {
     build_post_test();
+    rebuild_test();
     return 0;
 }
