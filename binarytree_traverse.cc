910fc0e35533be29378c0c7fb1c54a2b574d521a
wuyu201321060203 <wuyuangelysys@gmail.com>
2014-07-24 14:32:56 +0800

    (OJ)binary-tree traverse

---
+++ binarytree_traverse.cc
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
+typedef int TreeNodeElement;
+
+struct BinaryTreeNode
+{
+    BinaryTreeNode(BinaryTreeNode* left , BinaryTreeNode* right,
+                   TreeNodeElement value):left_(left),
+                                          right_(right),
+                                          value_(value)
+    {
+    }
+
+    BinaryTreeNode* left_;
+    BinaryTreeNode* right_;
+    TreeNodeElement value_;
+};
+
+typedef boost::function<void (BinaryTreeNode*)> VisitFunctor;
+
+void pre_order(BinaryTreeNode* root , VisitFunctor const& func)
+{
+    if(root == NULL)
+        return;
+    func(root);
+    pre_order(root->left_ , func);
+    pre_order(root->right_ , func);
+}
+
+void visit(BinaryTreeNode* node)
+{
+    printf("%d\n" , node->value_);
+}
+
+int main(void)
+{
+    BinaryTreeNode left(NULL , NULL , 2);
+    BinaryTreeNode right(NULL , NULL , 3);
+    BinaryTreeNode root(&left , &right , 1);
+    pre_order(&root , boost::bind(&visit , _1));
+    return 0;
+}
